014b2147c0bd8b8cf3d6c5f8f1c26f793d80f436
gonutz <lars.scheme@gmail.com>
2020-07-24 23:52:06 +0200

    Serve two dynamic files

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
-import "net/http"
+import (
+	"io/ioutil"
+	"net/http"
+)
 
 func main() {
+	http.HandleFunc("/hello/", func(w http.ResponseWriter, r *http.Request) {
+		data, _ := ioutil.ReadFile("served_files/hello_world.txt")
+		w.Write(data)
+	})
 	http.Handle("/", http.FileServer(http.Dir("served_files")))
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
